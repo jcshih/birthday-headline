1dbf8d4c41d295e6fbdbfad0b6876fdffcdd4bad
Jonathan Shih <joncshih@gmail.com>
2016-02-01 04:28:52 -0800

    form submission and style

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,41 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { findDOMNode } from 'react-dom';
 import style from './Form.css';
 
-const Form = (props) => {
-  return (
-    <div className={style.form}>
-      <button onClick={props.handleGetHeadlineClick}
-              disabled={props.isFetching}>
-        Form
-      </button>
-    </div>
-  );
+class Form extends Component {
+  clickHandler(e) {
+    e.preventDefault();
+    this.props.handleGetHeadlineClick();
+    findDOMNode(this.refs.month).value = "";
+    findDOMNode(this.refs.day).value = "";
+    findDOMNode(this.refs.year).value = "";
+    this.refs.button.focus();
+  }
+
+  render() {
+    return (
+      <form className={style.form}
+            onSubmit={this.clickHandler.bind(this)}>
+        <div className={style.birthdayInput}>
+          <input ref="month" type="text"
+                 style={{width: '2em'}} placeholder="mm"/>
+          {' '}
+          <input ref="day" type="text"
+                 style={{width: '2em'}} placeholder="dd"/>
+          {' '}
+          <input ref="year" type="text"
+                 style={{width: '4em'}} placeholder="yyyy"/>
+        </div>
+        <div>
+          <button ref="button"
+                  className={style.button}
+                  disabled={this.props.isFetching}>
+            See a newspaper headline from the day you were born!
+          </button>
+        </div>
+      </form>
+    );
+  }
 };
 
 Form.propTypes = {
