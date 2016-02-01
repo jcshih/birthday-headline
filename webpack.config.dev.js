8b7853b5dd34587fc907543f1ffaceb463bd55ed
Jonathan Shih <joncshih@gmail.com>
2016-01-31 23:27:56 -0800

    set up boilerplate

---
+++ webpack.config.dev.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/dev-server',
+    './src/index.jsx'
+  ],
+  output: {
+    path: __dirname,
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.tpl.html',
+      inject: 'body',
+      filename: 'index.html'
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loaders: ['babel'],
+      exclude: /node_modules/
+    }]
+  }
+};
