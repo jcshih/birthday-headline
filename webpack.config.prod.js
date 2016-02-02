335b450ee8be7ec71b9375e7d2a3cdb42468e8ad
Jonathan Shih <joncshih@gmail.com>
2016-02-02 00:11:35 -0800

    webpack production build config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -42,7 +42,12 @@ module.exports = {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
-      loader: 'babel'
+      loader: 'babel',
+      query: {
+        cacheDirectory: true,
+        plugins: ['transform-decorators-legacy' ],
+        presets: ['es2015', 'react', 'stage-2']
+      },
     }, {
       test: /\.json?$/,
       loader: 'json'
